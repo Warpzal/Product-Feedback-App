522da5cdb9f4bf98fb8c9505912feeffcc772801
Micheaaa <michea@michea.com>
2021-10-05 14:37:02 -0500

    Register custom fields

--- wordpress/wp-content/plugins/product-feeback/src/PostTypes/SuggestionPostType.php
+++ wordpress/wp-content/plugins/product-feeback/src/PostTypes/SuggestionPostType.php
@@ -2,28 +2,79 @@
 
 namespace ProductFeedbackPlugin\PostTypes;
 
-use ProductFeedbackPlugin\Utils\PostTypeLabelUtility;
+use ProductFeedbackPlugin\Utils\PostTypeUtility;
 use ProductFeedbackPlugin\Interfaces\Hookable;
 
 class SuggestionPostType implements Hookable {
-    use PostTypeLabelUtility;
+    use PostTypeUtility;
 
     const SLUG = 'suggestion';
     const GRAPHQL_SINGLE_NAME = 'Suggestion';
     const GRAPHQL_PLURAL_NAME = 'Suggestions';
 
+    const FIELDS = [
+        'detail' => ['type' => 'String'],
+        'upvotes' => ['type' => 'Int']
+    ];
+
     public function register_hooks() {
         add_action('init', [$this, 'register']);
+        add_action('graphql_register_types', [$this, 'register_fields']);
     }
 
-    public function register() {
+    public function register() : void {
+        $this->register_type();
+        $this->register_field_groups();
+    }
+
+    public function register_type() : void {
         register_post_type(self::SLUG, [
 			'labels'              => $this->generate_labels(self::GRAPHQL_SINGLE_NAME, self::GRAPHQL_PLURAL_NAME),
             'public'              => true,
-            'supports'            => ['title', 'comments'],
+            'supports'            => ['title', 'comments', 'author'],
             'show_in_graphql'     => true,
             'graphql_single_name' => self::GRAPHQL_SINGLE_NAME,
             'graphql_plural_name' => self::GRAPHQL_PLURAL_NAME,
 		]);
+    }
+
+    public function register_field_groups() : void {
+        acf_add_local_field_group([
+            'active' => true,
+            'title' => 'Suggestion Fields',
+            'key' => 'suggestion_fields',
+            'fields' => [
+                [
+                    'key' => 'suggestion_detail_field',
+                    'label' => 'Detail',
+                    'name' => 'detail',
+                    'type' => 'textarea',
+                    'required' => 1,
+                ],
+                [
+                    'key' => 'suggestion_upvotes_field',
+                    'label' => 'Upvotes',
+                    'name' => 'upvotes',
+                    'type' => 'number',
+                    'required' => 0,
+                    'default_value' => 0,
+                    'min' => 0,
+                ]
+            ],
+            'location' => [[
+                [
+                    'param' => 'post_type',
+                    'operator' => '==',
+                    'value' => 'suggestion',
+                ]
+            ]]
+        ]);
+    }
+
+    public function register_fields() : void {
+        register_graphql_fields(
+            self::GRAPHQL_SINGLE_NAME,
+            $this->generate_fields(self::GRAPHQL_SINGLE_NAME, self::FIELDS)
+        );
     }
 }
---
+++ wordpress/wp-content/plugins/product-feeback/src/Utils/PostTypeUtility.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ProductFeedbackPlugin\Utils;
+
+trait PostTypeUtility {
+	static $text_domain = 'product-feedback-app';
+
+    /**
+     * Generate labels for a post type.
+	 *
+	 * @param string $singular          Uppercase, singular label.
+	 * @param string $plural            Uppercase, plural label.
+	 * @param array  $additional_labels Additional labels.
+	 *
+	 * @return array Labels.
+     */
+	protected function generate_labels(string $singular, string $plural, array $additional_labels = []) : array {
+		$labels = [
+			'name'                  => _x($plural, 'post type general name', self::$text_domain),
+			'singular_name'         => _x($singular, 'post type singular name', self::$text_domain),
+			'menu_name'             => _x($plural, 'admin menu', self::$text_domain),
+			'name_admin_bar'        => _x($singular, 'add new on admin bar', self::$text_domain),
+			'add_new'               => _x('Add New', $singular, self::$text_domain),
+			'add_new_item'          => __("Add New {$singular}", self::$text_domain),
+			'new_item'              => __("New {$singular}", self::$text_domain),
+			'edit_item'             => __("Edit {$singular}", self::$text_domain),
+			'view_item'             => __("View {$singular}", self::$text_domain),
+			'all_items'             => __("All {$plural}", self::$text_domain),
+			'search_items'          => __("Search {$plural}", self::$text_domain),
+			'parent_item_colon'     => __("Parent {$plural}:", self::$text_domain),
+			'not_found'             => __("No {$plural} found.", self::$text_domain),
+			'not_found_in_trash'    => __("No {$plural} found in Trash.", self::$text_domain),
+			'archives'              => __("{$singular} Archives", self::$text_domain),
+            'update_item'           => __("Update {$singular}", self::$text_domain),
+            'insert_into_item'      => __("Insert into {$singular}", self::$text_domain),
+            'uploaded_to_this_item' => __("Uploaded to this {$singular}", self::$text_domain),
+            'items_list'            => __("{$plural} list", self::$text_domain),
+            'items_list_navigation' => __("{$plural} list navigation", self::$text_domain),
+            'filter_items_list'     => __("Filter {$plural} list", self::$text_domain),
+	    ];
+        
+		return array_merge($labels, $additional_labels);
+	}
+
+	/**
+     * Generate fields for a post type.
+	 *
+	 * @param string $singular          Uppercase, singular label.
+	 * @param array  $post_type_fields  Post type fields.
+	 *
+	 * @return array Fields.
+     */
+	protected function generate_fields(string $singular, array $post_type_fields) : array {
+		$fields = [];
+
+		foreach ($post_type_fields as $field => $details) {
+			$fields[$field] = [
+				'type'        => $details['type'] ?: 'String',
+				'description' => __("{$singular} {$field}", self::$text_domain),
+				'resolve'     => function($suggestion) use ($field) {
+					$value = get_field($field, $suggestion->ID);
+					return $value ?: null;
+				}
+			];
+		}
+
+		return $fields;
+	}
+}
