5413c5f2e63a29d99e002a0a3856b1e091a42c12
yanyamz <hello.anthony9898@gmail.com>
2021-10-07 18:59:25 -0700

    Homepage complete

--- product-feedback-app/components/Suggestion/Suggestion.jsx
+++ product-feedback-app/components/Suggestion/Suggestion.jsx
@@ -4,32 +4,34 @@ import { faAngleUp, faComment } from '@fortawesome/free-solid-svg-icons' // impo
 function Suggestion({ title, description, tags, likes, comments }) {
 	return (
 		<div className="suggestion card is-rounded is-block">
-			<p className="suggestion__title">{title}</p>
-			<p className="suggestion__description">{description}</p>
-			<div className="suggestion__tags">
-				{tags.map((tag) => {
-					return (
-						<div className="button button--tag suggestion__tag">
-							{tag}
-						</div>
-					)
-				})}
-			</div>
-			<div className="suggestion__bottom-bar">
-				<div className="button">
+			<div className="suggestion__likes">
+				<div className="suggestion__button button">
 					<FontAwesomeIcon
 						className="suggestion__angleUp"
 						icon={faAngleUp}
 					></FontAwesomeIcon>
 					<span className="suggestion__likes">{likes}</span>
 				</div>
-				<div className="suggestion__comments">
-					<FontAwesomeIcon
-						className="suggestion__commentIcon"
-						icon={faComment}
-					></FontAwesomeIcon>
-					<div className="suggestion__comments-num">{comments}</div>
+			</div>
+			<div className="suggestion__main-content">
+				<p className="suggestion__title">{title}</p>
+				<p className="suggestion__description">{description}</p>
+				<div className="suggestion__tags">
+					{tags.map((tag) => {
+						return (
+							<div className="button button--tag suggestion__tag">
+								{tag}
+							</div>
+						)
+					})}
 				</div>
+			</div>
+			<div className="suggestion__comments">
+				<FontAwesomeIcon
+					className="suggestion__commentIcon"
+					icon={faComment}
+				></FontAwesomeIcon>
+				<div className="suggestion__comments-num">{comments}</div>
 			</div>
 		</div>
 	)
